db78f114c4cfd3ef6155d695b7d2f6daa20120e9
WongYC19 <ycfkjc@Hotmail.com>
2022-04-25 21:42:25 +0800

    Fix staticfiles_dirs

--- digital_resume/digital_resume/settings.py
+++ digital_resume/digital_resume/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / "static", ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/images/"
