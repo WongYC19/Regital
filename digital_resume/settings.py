e34feb2420452bef2b2f68f6040743bf5af54faa
WongYC19 <ycfkjc@Hotmail.com>
2022-04-29 00:33:16 +0800

    Settings file

--- digital_resume/settings.py
+++ digital_resume/settings.py
@@ -84,7 +84,7 @@ ROOT_URLCONF = 'digital_resume.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         
         'APP_DIRS': True,
         'OPTIONS': {
