da9459a69f580db30bb481d615c9a17b35861430
WongYC19 <ycfkjc@Hotmail.com>
2022-04-29 20:58:14 +0800

    Update static dirs"

--- digital_resume/settings.py
+++ digital_resume/settings.py
@@ -158,11 +158,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
-STATICFILES_DIRS = (STATIC_ROOT, )
+STATICFILES_DIRS = (STATIC_ROOT, os.path.join(BASE_DIR, "build/static"))
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/images/"
