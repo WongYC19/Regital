c885dbe8cb34004ef208941d16102f325cbc2419
WongYC19 <ycfkjc@Hotmail.com>
2022-04-30 00:19:56 +0800

    Update base dir

--- digital_resume/settings.py
+++ digital_resume/settings.py
@@ -19,7 +19,7 @@ from datetime import timedelta
 mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
