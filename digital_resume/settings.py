3b33157c41d11fc980cc4fea0f093dbaa64a040d
WongYC19 <ycfkjc@Hotmail.com>
2022-04-30 00:51:31 +0800

    Update static dirs

--- digital_resume/settings.py
+++ digital_resume/settings.py
@@ -19,7 +19,7 @@ from datetime import timedelta
 mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -84,8 +84,7 @@ ROOT_URLCONF = 'digital_resume.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
-        
+        'DIRS': [os.path.join(BASE_DIR, 'build'), BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
