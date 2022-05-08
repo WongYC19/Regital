a136dca8a264945f1530a2d193a41048a7e6796a
WongYC19 <ycfkjc@Hotmail.com>
2022-05-08 23:18:42 +0800

    Update fav icon url

--- digital_resume/urls.py
+++ digital_resume/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import path, include
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-# from rest_framework_jwt.views import obtain_jwt_token
 
 react_urls = ["resume", "shared_resume", "shared_resume/view/<str:uid>", "resume/<str:uid>", "profile", "view/<str:uid>", "login", "signup", "password_reset", "password_reset_confirm", "password_reset_confirm/<str:uid>/<str:token>", "password_reset_done"]
 react_url_patterns = [path(url + "/", TemplateView.as_view(template_name="build/index.html")) for url in react_urls]
@@ -29,6 +29,10 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="build/index.html")),
     path("", include("api.urls")),
     path("", include("resume.urls")),    
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
 
 urlpatterns += react_url_patterns
